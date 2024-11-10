e099cc43ca565f69fca256bba77ee67b6469f2ea
hyphen <lhfcjhyy@gmail.com>
2024-11-10 16:40:15 +0800

    fix side button for raspberry pi

--- back.py
+++ back.py
@@ -8,12 +8,21 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 backlit_on=False
+init_label=False
+last_time=time.time()
 def signal_handler(sig, frame):
     GPIO.cleanup()
     print("cleanup")
     sys.exit(0)
 def button_pressed_callback(channel):
+    global init_label
+    global last_time
     global backlit_on
+    if time.time()-last_time<0.2:
+        return
+    last_time=time.time()
+    if not init_label:
+        return
     if backlit_on:
         backlit_on=False
         os.system("echo 0 | sudo tee /sys/module/jdi_drm/parameters/backlit > /dev/null")
@@ -29,6 +38,7 @@ if __name__ == '__main__':
     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(17, GPIO.RISING, 
             callback=button_pressed_callback, bouncetime=100)
+    init_label=True
     
     signal.signal(signal.SIGINT, signal_handler)
     signal.pause()
